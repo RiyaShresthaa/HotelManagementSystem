17746200aa454a1f3bfc2ddf898999346ffbd0a7
RiyaShresthaa <riyashrestha279@gmail.com>
2023-08-17 22:28:35 +0545

    models made and serializer file created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# choices for user_type.
+user_type_list = [('Frontdesk','Frontdesk'),('Accounting','Accounting'),('Restaurant','Restaurant'),('Management','Management')]
+
+class User(AbstractUser):#inherite abstract user in User class
+    #columns for User table 
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=200)
+    username = models.CharField(max_length=200,default='User',null=True)
+    user_type = models.CharField(max_length=20)
+
+    #to make the email field the required login creditials to login the system
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
