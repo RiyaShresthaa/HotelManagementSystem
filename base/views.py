262e6b74834db3b12a89b3125d488942a667e877
RiyaShresthaa <riyashrestha279@gmail.com>
2023-08-18 09:18:11 +0545

    home url added

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def home(request):
+    return Response({"detail":'HELLO'})
+
+
